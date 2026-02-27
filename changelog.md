1a2e9dc79b9ec78ad85cf0c8dbaa88a7de61ba46
MehVahdJukaar <matteogozzi1998@gmail.com>
2026-02-27 08:28:53 +0100

    stuff

--- changelog.md
+++ changelog.md
@@ -1,1 +1,3 @@
-- downgraded java to 17
+some fixes. thanks SupperDupper.
+
+Added a workaround for sodium embeddium due to them using an incompatible mixin type. note that camera views will likely work wonky with those on due to this.
--- fabric/src/main/java/net/mehvahdjukaar/vista/integration/computer_craft/fabric/CCCompatImpl.java
+++ fabric/src/main/java/net/mehvahdjukaar/vista/integration/computer_craft/fabric/CCCompatImpl.java
@@ -1,18 +1,9 @@
 package net.mehvahdjukaar.vista.integration.computer_craft.fabric;
 
-import dan200.computercraft.api.peripheral.IPeripheral;
-import dan200.computercraft.api.peripheral.PeripheralLookup;
-import net.mehvahdjukaar.vista.VistaMod;
-import net.mehvahdjukaar.vista.integration.computer_craft.ViewFinderPeripheral;
 
 public class CCCompatImpl {
 
     public static void setup() {
-        PeripheralLookup.get().registerForBlockEntity((tile, direction) -> {
-            if (tile.ccPeripheral == null) tile.ccPeripheral = new ViewFinderPeripheral(tile);
-            return (IPeripheral) tile.ccPeripheral;
-        }, VistaMod.VIEWFINDER_TILE.get());
-
     }
 
     public static void init() {
